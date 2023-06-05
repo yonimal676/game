798dd884ba85ec99d98b5f4f78df72fe101b0292
yonimal676 <malkayehonatan@gmail.com>
2023-06-05 20:21:58 +0300

    waves work

--- app/src/main/java/com/example/game/Background.java
+++ app/src/main/java/com/example/game/Background.java
@@ -6,16 +6,12 @@ import android.graphics.BitmapFactory;
 
 public class Background
 {
-    Bitmap backgroundBitmap, devBackgroundBitmap;
+    Bitmap backgroundBitmap;
 
     public Background(Resources res, int screenX, int screenY, byte metersInTheScreen)
     {
         backgroundBitmap = BitmapFactory.decodeResource(res, R.drawable.background);
         backgroundBitmap = Bitmap.createScaledBitmap(backgroundBitmap, screenX, screenY, false);
-
-        devBackgroundBitmap = BitmapFactory.decodeResource(res, R.drawable.black);
-        devBackgroundBitmap = Bitmap.createScaledBitmap(devBackgroundBitmap, screenX, screenY, false);
-
 
     }
 }
--- app/src/main/java/com/example/game/MainActivity.java
+++ app/src/main/java/com/example/game/MainActivity.java
@@ -2,14 +2,25 @@ package com.example.game;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.game.GameActivity;
+/*
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+*/
+
 
 public class MainActivity extends AppCompatActivity
 {
+
+    EditText nameEditText;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -26,6 +37,20 @@ public class MainActivity extends AppCompatActivity
 
 
 
+//        nameEditText = findViewById(R.id)
+
 
     }
+
+
+/*    public void saveName(View view)
+    {
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("name");
+
+        myRef.setValue("Hello, World!");
+    }*/
+
+
 }
